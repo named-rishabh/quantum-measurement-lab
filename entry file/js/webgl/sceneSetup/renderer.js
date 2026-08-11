61353a8a60bc34eebce9d7d73f398169d13d422d
named-rishabh <rishabhyadav.dev@gmail.com>
2026-08-11 14:55:52 +0530

    added fps status

--- entry file/js/webgl/sceneSetup/renderer.js
+++ entry file/js/webgl/sceneSetup/renderer.js
@@ -1,6 +1,7 @@
 // Draws the scene onto the canvas every frame.
 
 import {WebGLRenderer} from "three";
+import Stats from "three/examples/jsm/libs/stats.module.js";
 
 export default class Renderer {
     constructor(experience) {
@@ -9,6 +10,9 @@ export default class Renderer {
         this.height = this.experience.wrapper.getBoundingClientRect().height;
         this.scene = experience.scene;
         this.camera = experience.camera.camera;
+        this.stats = new Stats();
+        this.stats.showPanel(0); // 0: fps, 1: ms, 2: mb
+        document.body.appendChild(this.stats.dom);
         this.renderer = new WebGLRenderer({
             canvas: experience.canvas,
             antialias: true
@@ -34,9 +38,11 @@ export default class Renderer {
     }
 
     update() {
+        this.stats.begin();
         this.renderer.render(
             this.scene,
             this.camera
         );
+        this.stats.end();
     }
 }
