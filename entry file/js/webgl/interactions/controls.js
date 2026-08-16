be6b1b8de667f48a8eb2440e97361ebc332ea639
named-rishabh <rishabhyadav.dev@gmail.com>
2026-08-16 20:00:46 +0530

    modified controls

--- entry file/js/webgl/interactions/controls.js
+++ entry file/js/webgl/interactions/controls.js
@@ -27,11 +27,11 @@ export default class Controls {
         this.controls2.noPan = true;
         this.controls2.noZoom = false;
         this.controls2.zoomSpeed = 7.5;
-        this.controls2.maxDistance = 65;
+        this.controls2.maxDistance = 45;
         this.controls2.minDistance = 3;
         
-        const minPan = new Vector3(-10, 0, 0);
-        const maxPan = new Vector3(10, 0, 0);
+        const minPan = new Vector3(-15, -5, 0);
+        const maxPan = new Vector3(15, 10, 0);
 
         this.controls.addEventListener('change', () => {
             this.controls.target.clamp(minPan, maxPan);
